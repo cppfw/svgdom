e343d4d17f7993c65c10a78e5518403d6b47f024
Ivan Gagis <igagis@gmail.com>
2017-07-26 19:05:38 +0300

    refactoring

--- src/svgdom/dom.cpp
+++ src/svgdom/dom.cpp
@@ -808,82 +808,8 @@ std::ostream& operator<<(std::ostream& s, const Length& l){
 
 
 
-void SvgElement::toStream(std::ostream& s, unsigned indent) const{
-	auto ind = indentStr(indent);
-	
-	s << ind << "<svg";
-	if(indent == 0){//if outermost "svg" element
-		s << " xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\"";
-	}
-	this->attribsToStream(s);
-	
-	if(this->children.size() == 0){
-		s << "/>";
-	}else{
-		s << ">" << std::endl;
-		this->childrenToStream(s, indent + 1);
-		s << ind << "</svg>";
-	}
-	s << std::endl;
-}
-
-void SymbolElement::toStream(std::ostream& s, unsigned indent) const {
-	auto ind = indentStr(indent);
-
-	s << ind << "<symbol";
-	
-	this->attribsToStream(s);
-
-	if (this->children.size() == 0) {
-		s << "/>";
-	}else{
-		s << ">" << std::endl;
-		this->childrenToStream(s, indent + 1);
-		s << ind << "</symbol>";
-	}
-	s << std::endl;
-}
 
 
-
-
-
-
-
-void DefsElement::toStream(std::ostream& s, unsigned indent) const{
-	auto ind = indentStr(indent);
-	
-//	TRACE(<< "DefsElement::toStream():" << std::endl)
-
-	s << ind << "<defs";
-	this->Container::attribsToStream(s);
-	this->Transformable::attribsToStream(s);
-	this->Styleable::attribsToStream(s);
-	
-	if(this->children.size() == 0){
-		s << "/>";
-	}else{
-		s << ">" << std::endl;
-		this->childrenToStream(s, indent + 1);
-		s << ind << "</defs>";
-	}
-	s << std::endl;
-}
-
-void UseElement::toStream(std::ostream& s, unsigned indent) const {
-	auto ind = indentStr(indent);
-
-//	TRACE(<< "UseElement::toStream():" << std::endl)
-
-	s << ind << "<use";
-	this->Element::attribsToStream(s);
-	this->Transformable::attribsToStream(s);
-	this->Styleable::attribsToStream(s);
-	this->Rectangle::attribsToStream(s);
-	this->Referencing::attribsToStream(s);
-	s << "/>";
-	s << std::endl;
-}
 
 
 
@@ -2127,18 +2053,7 @@ decltype(SvgElement::viewBox) ViewBoxed::parseViewbox(const std::string& str) {
 
 
 
-void SvgElement::attribsToStream(std::ostream& s) const {
-	this->Container::attribsToStream(s);
-	this->Styleable::attribsToStream(s);
-	this->Rectangle::attribsToStream(s);
-	this->ViewBoxed::attribsToStream(s);
-}
 
-void SymbolElement::attribsToStream(std::ostream& s) const {
-	this->Container::attribsToStream(s);
-	this->Styleable::attribsToStream(s);
-	this->ViewBoxed::attribsToStream(s);
-}
 
 real Length::toPx(real dpi) const noexcept{
 	switch(this->unit){
@@ -2181,17 +2096,4 @@ std::unique_ptr<SvgElement> svgdom::load(std::string& s){
 	return ::load(doc);
 }
 
-real SvgElement::aspectRatio(real dpi)const{
-	real w = this->width.toPx(dpi);
-	real h = this->height.toPx(dpi);
-	
-	if(w <= 0 || h <= 0){
-		if(this->viewBox[2] <= 0 || this->viewBox[3] <= 0){
-			return 0;
-		}else{
-			return this->viewBox[2] / this->viewBox[3];
-		}
-	}
-	
-	return w / h;
-}
+
