a3c400f5e05530de20ae28188445048875feccc0
Ivan Gagis <igagis@gmail.com>
2017-07-26 18:38:04 +0300

    refactoring

--- src/svgdom/dom.hpp
+++ src/svgdom/dom.hpp
@@ -16,6 +16,7 @@
 #include "elements/Container.hpp"
 #include "elements/Referencing.hpp"
 #include "elements/Transformable.hpp"
+#include "elements/ViewBoxed.hpp"
 
 
 namespace svgdom{
@@ -68,45 +69,7 @@ struct UseElement :
 	void accept(Visitor& visitor)const override;
 };
 
-//TODO: move to ViewBoxed
-enum class PreserveAspectRatio_e{
-	NONE,
-	X_MIN_Y_MIN,
-	X_MID_Y_MIN,
-	X_MAX_Y_MIN,
-	X_MIN_Y_MID,
-	X_MID_Y_MID,
-	X_MAX_Y_MID,
-	X_MIN_Y_MAX,
-	X_MID_Y_MAX,
-	X_MAX_Y_MAX
-};
 
-/**
- * @brief SVG element which has viewBox attribute.
- * Provides handling of viewBox and preserveAspectRatio attributes.
- */
-struct ViewBoxed {
-	std::array<real, 4> viewBox = {{-1, -1, -1, -1}};
-
-	struct {
-		PreserveAspectRatio_e preserve = PreserveAspectRatio_e::NONE;
-		bool defer = false;
-		bool slice = false;
-	} preserveAspectRatio;
-
-	//TODO: move to cpp
-	void parseAndFillPreserveAspectRatio(const std::string& str);
-
-	//TODO: move to cpp
-	static decltype(viewBox) parseViewbox(const std::string& str);
-
-	void attribsToStream(std::ostream& s)const;
-
-	bool isViewBoxSpecified()const {
-		return this->viewBox[2] >= 0;//width is not negative
-	}
-};
 
 struct SvgElement :
 		public Container,
---
+++ src/svgdom/elements/ViewBoxed.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <array>
+
+#include "../config.hpp"
+
+namespace svgdom{
+
+//TODO: move to ViewBoxed
+enum class PreserveAspectRatio_e{
+	NONE,
+	X_MIN_Y_MIN,
+	X_MID_Y_MIN,
+	X_MAX_Y_MIN,
+	X_MIN_Y_MID,
+	X_MID_Y_MID,
+	X_MAX_Y_MID,
+	X_MIN_Y_MAX,
+	X_MID_Y_MAX,
+	X_MAX_Y_MAX
+};
+
+/**
+ * @brief SVG element which has viewBox attribute.
+ * Provides handling of viewBox and preserveAspectRatio attributes.
+ */
+struct ViewBoxed {
+	std::array<real, 4> viewBox = {{-1, -1, -1, -1}};
+
+	struct {
+		PreserveAspectRatio_e preserve = PreserveAspectRatio_e::NONE;
+		bool defer = false;
+		bool slice = false;
+	} preserveAspectRatio;
+
+	//TODO: move to cpp
+	void parseAndFillPreserveAspectRatio(const std::string& str);
+
+	//TODO: move to cpp
+	static decltype(viewBox) parseViewbox(const std::string& str);
+
+	void attribsToStream(std::ostream& s)const;
+
+	bool isViewBoxSpecified()const {
+		return this->viewBox[2] >= 0;//width is not negative
+	}
+};
+
+}
