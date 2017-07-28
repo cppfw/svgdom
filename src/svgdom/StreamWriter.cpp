f03122d14c503d693c6515af38eb4a90c1cb1a1b
Ivan Gagis <igagis@gmail.com>
2017-07-28 15:50:05 +0300

    refactoring

---
+++ src/svgdom/StreamWriter.cpp
@@ -0,0 +1,205 @@
+#include "StreamWriter.hxx"
+
+#include <utki/util.hpp>
+
+#include "util.hxx"
+
+using namespace svgdom;
+
+void StreamWriter::childrenToStream(const Container& e) {
+	++this->indent;
+	utki::ScopeExit scopeExit([this]() {
+		--this->indent;
+	});
+	for (auto& c : e.children) {
+		c->accept(*this);
+	}
+}
+
+
+void StreamWriter::visit(const GElement& e) {
+	auto ind = indentStr(this->indent);
+
+	this->s << ind << "<g";
+	e.attribsToStream(this->s);
+
+	if (e.children.size() == 0) {
+		this->s << "/>";
+	} else {
+		this->s << ">" << std::endl;
+		this->childrenToStream(e);
+		this->s << ind << "</g>";
+	}
+	this->s << std::endl;
+}
+
+void StreamWriter::visit(const SvgElement& e) {
+	auto ind = indentStr(this->indent);
+
+	this->s << ind << "<svg";
+	if (this->indent == 0) {//if outermost "svg" element
+		this->s << " xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\"";
+	}
+	e.attribsToStream(this->s);
+
+	if (e.children.size() == 0) {
+		s << "/>";
+	} else {
+		s << ">" << std::endl;
+		this->childrenToStream(e);
+		s << ind << "</svg>";
+	}
+	s << std::endl;
+}
+
+void StreamWriter::visit(const LineElement& e) {
+	auto ind = indentStr(indent);
+
+	s << ind << "<line";
+	e.attribsToStream(s);
+	s << "/>";
+	s << std::endl;
+}
+
+void StreamWriter::visit(const RectElement& e) {
+	auto ind = indentStr(indent);
+
+	s << ind << "<rect";
+	e.attribsToStream(s);
+	s << "/>";
+	s << std::endl;
+}
+
+void StreamWriter::visit(const EllipseElement& e) {
+	auto ind = indentStr(indent);
+
+	s << ind << "<ellipse";
+	e.attribsToStream(s);
+	s << "/>";
+	s << std::endl;
+}
+
+void StreamWriter::visit(const PolygonElement& e) {
+	auto ind = indentStr(indent);
+
+	s << ind << "<polygon";
+	e.attribsToStream(s);
+	s << "/>";
+	s << std::endl;
+}
+
+void StreamWriter::visit(const PolylineElement& e) {
+	auto ind = indentStr(indent);
+
+	s << ind << "<polyline";
+	e.attribsToStream(s);
+	s << "/>";
+	s << std::endl;
+}
+
+void StreamWriter::visit(const CircleElement& e) {
+	auto ind = indentStr(indent);
+
+	s << ind << "<circle";
+	e.attribsToStream(s);
+	s << "/>";
+	s << std::endl;
+}
+
+void StreamWriter::visit(const PathElement& e) {
+	auto ind = indentStr(indent);
+
+	s << ind << "<path";
+
+	e.attribsToStream(s);
+
+	s << "/>";
+	s << std::endl;
+}
+
+void StreamWriter::visit(const UseElement& e) {
+	auto ind = indentStr(indent);
+
+	s << ind << "<use";
+	e.Element::attribsToStream(s);
+	e.Transformable::attribsToStream(s);
+	e.Styleable::attribsToStream(s);
+	e.Rectangle::attribsToStream(s);
+	e.Referencing::attribsToStream(s);
+	s << "/>";
+	s << std::endl;
+}
+
+void StreamWriter::visit(const Gradient::StopElement& e) {
+	auto ind = indentStr(this->indent);
+	this->s << ind << "<stop";
+	e.attribsToStream(this->s);
+	this->s << "/>" << std::endl;
+}
+
+void StreamWriter::visit(const RadialGradientElement& e) {
+	auto ind = indentStr(this->indent);
+
+	this->s << ind << "<radialGradient";
+	e.attribsToStream(this->s);
+
+	if (e.children.size() == 0) {
+		this->s << "/>";
+	} else {
+		this->s << ">" << std::endl;
+		this->childrenToStream(e);
+		this->s << ind << "</radialGradient>";
+	}
+	this->s << std::endl;
+}
+
+void StreamWriter::visit(const LinearGradientElement& e) {
+	auto ind = indentStr(this->indent);
+
+	this->s << ind << "<linearGradient";
+	e.attribsToStream(this->s);
+
+	if (e.children.size() == 0) {
+		this->s << "/>";
+	} else {
+		this->s << ">" << std::endl;
+		this->childrenToStream(e);
+		this->s << ind << "</linearGradient>";
+	}
+	this->s << std::endl;
+}
+
+void StreamWriter::visit(const DefsElement& e) {
+	auto ind = indentStr(this->indent);
+
+	this->s << ind << "<defs";
+	e.Element::attribsToStream(s);
+	e.Transformable::attribsToStream(s);
+	e.Styleable::attribsToStream(s);
+
+	if (e.children.size() == 0) {
+		s << "/>";
+	} else {
+		s << ">" << std::endl;
+		this->childrenToStream(e);
+		s << ind << "</defs>";
+	}
+	s << std::endl;
+}
+
+void StreamWriter::visit(const SymbolElement& e) {
+	auto ind = indentStr(this->indent);
+
+	this->s << ind << "<symbol";
+
+	e.attribsToStream(this->s);
+
+	if (e.children.size() == 0) {
+		s << "/>";
+	} else {
+		s << ">" << std::endl;
+		this->childrenToStream(e);
+		s << ind << "</symbol>";
+	}
+	s << std::endl;
+}
---
+++ src/svgdom/StreamWriter.hxx
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Visitor.hpp"
+
+namespace svgdom{
+
+class StreamWriter : public Visitor{
+	std::ostream& s;
+	
+	unsigned indent = 0;
+	
+	void childrenToStream(const Container& e);
+	
+public:
+	StreamWriter(std::ostream& s) : s(s) {}
+	
+	void visit(const GElement& e) override;
+	void visit(const SvgElement& e) override;
+	void visit(const SymbolElement& e) override;
+	void visit(const DefsElement& e) override;
+	void visit(const LinearGradientElement& e) override;
+	void visit(const RadialGradientElement& e) override;
+	void visit(const Gradient::StopElement& e) override;
+	void visit(const UseElement& e) override;
+	void visit(const PathElement& e) override;
+	void visit(const CircleElement& e) override;
+	void visit(const PolylineElement& e) override;
+	void visit(const PolygonElement& e) override;
+	void visit(const EllipseElement& e) override;
+	void visit(const RectElement& e) override;
+	void visit(const LineElement& e) override;
+};
+
+}
