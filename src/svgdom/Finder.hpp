89936250bff9ff7b7aa34eb84d7f35135b042f32
Ivan Gagis <igagis@gmail.com>
2017-08-09 17:24:31 +0300

    Finder and StyleStack added

---
+++ src/svgdom/Finder.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <map>
+
+#include "elements/Element.hpp"
+
+#include "StyleStack.hpp"
+
+namespace svgdom{
+
+class Finder{
+	
+	const svgdom::Element& root;
+	
+public:
+	
+	Finder(const svgdom::Element& root);
+	
+	struct ElementInfo{
+		const svgdom::Element* e = nullptr;
+		StyleStack ss;
+		
+		explicit operator bool()const{
+			return this->e;
+		}
+	};
+	
+	const ElementInfo& findById(const std::string& id);
+	
+	void clearCache();
+	
+private:
+	std::map<std::string, ElementInfo> cache;
+};
+	
+}
