b197843543428ab0cee464af40107b1f25155d63
Ivan Gagis <igagis@gmail.com>
2017-08-17 13:15:14 +0300

    release 0.2.73

--- src/svgdom/elements/Container.hpp
+++ src/svgdom/elements/Container.hpp
@@ -11,6 +11,16 @@ namespace svgdom{
  */
 struct Container{
 	std::list<std::unique_ptr<Element>> children;
+	
+	Container(){}
+	
+	/**
+	 * @brief Copy constructor.
+	 * This copy constructor does nothing because to make a deep copy of the children
+	 * one needs to clone every child by hand.
+	 * @param orig - object to copy.
+	 */
+	Container(const Container& orig){}
 };
 
 }
