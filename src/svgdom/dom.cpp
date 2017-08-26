7c0324b5755a36b584b7168b7da4efb494524b40
Ivan Gagis <igagis@gmail.com>
2017-08-26 23:20:30 +0300

    stuff

--- src/svgdom/dom.cpp
+++ src/svgdom/dom.cpp
@@ -56,17 +56,3 @@ std::unique_ptr<SvgElement> svgdom::load(std::istream& s){
 std::unique_ptr<SvgElement> svgdom::load(const std::string& s){
 	return load(utki::wrapBuf(s.c_str(), s.length()));
 }
-
-
-std::unique_ptr<SvgElement> svgdom::load(const utki::Buf<std::uint8_t> buf){
-	return load(utki::wrapBuf(reinterpret_cast<const char*>(&*buf.begin()), buf.size()));
-}
-
-std::unique_ptr<SvgElement> svgdom::load(const utki::Buf<char> buf){
-	Parser parser;
-
-	parser.feed(buf);
-	parser.end();
-
-	return parser.getDom();
-}
