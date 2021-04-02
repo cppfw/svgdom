edb6caf2689e56b0ada7ed0a8ccb612d6740efe9
JaimeIvanCervantes <jimmycc80@hotmail.com>
2021-04-01 22:02:35 -0700

    Simplified cache visitor on element_by_id_finder and elements_by_tag_name_finder

--- src/svgdom/util/element_by_id_finder.cpp
+++ src/svgdom/util/element_by_id_finder.cpp
@@ -17,73 +17,9 @@ private:
 
 public:
 	std::unordered_map<std::string, const element*> cache;
-	
-	void visit_container(const svgdom::element& e, const svgdom::container& c, const svgdom::styleable& s){
+
+	void default_visit(const element& e){
 		this->add_to_cache(e);
-		this->relay_accept(c);
-	}
-	void visit_element(const svgdom::element& e, const svgdom::styleable& s){
-		this->add_to_cache(e);
-	}
-	
-	void default_visit(const svgdom::element& e)override{
-		this->add_to_cache(e);
-	}
-	
-	void visit(const svgdom::g_element& e) override{
-		this->visit_container(e, e, e);
-	}
-	void visit(const svgdom::symbol_element& e) override{
-		this->visit_container(e, e, e);
-	}
-	void visit(const svgdom::svg_element& e) override{
-		this->visit_container(e, e, e);
-	}
-	void visit(const svgdom::radial_gradient_element& e) override{
-		this->visit_container(e, e, e);
-	}
-	void visit(const svgdom::linear_gradient_element& e) override{
-		this->visit_container(e, e, e);
-	}
-	void visit(const svgdom::defs_element& e) override{
-		this->visit_container(e, e, e);
-	}
-	void visit(const svgdom::filter_element& e) override{
-		this->visit_container(e, e, e);
-	}
-	
-	void visit(const svgdom::polyline_element& e) override{
-		this->visit_element(e, e);
-	}
-	void visit(const svgdom::circle_element& e) override{
-		this->visit_element(e, e);
-	}
-	void visit(const svgdom::use_element& e) override{
-		this->visit_element(e, e);
-	}
-	void visit(const svgdom::gradient::stop_element& e) override{
-		this->visit_element(e, e);
-	}
-	void visit(const svgdom::path_element& e) override{
-		this->visit_element(e, e);
-	}
-	void visit(const svgdom::rect_element& e) override{
-		this->visit_element(e, e);
-	}
-	void visit(const svgdom::line_element& e) override{
-		this->visit_element(e, e);
-	}
-	void visit(const svgdom::ellipse_element& e) override{
-		this->visit_element(e, e);
-	}
-	void visit(const svgdom::polygon_element& e) override{
-		this->visit_element(e, e);
-	}	
-	void visit(const svgdom::fe_gaussian_blur_element& e) override{
-		this->visit_element(e, e);
-	}
-	void visit(const image_element& e) override{
-		this->visit_element(e, e);
 	}
 };
 }
