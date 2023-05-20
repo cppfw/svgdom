3cc620d304ef4e53acf4fab62c52f518fb9c5b5e
Ivan Gagis <igagis@gmail.com>
2023-05-20 12:38:05 +0300

    fix lint issues

--- src/svgdom/util/casters.hpp
+++ src/svgdom/util/casters.hpp
@@ -35,12 +35,12 @@ namespace svgdom {
 
 // TODO: doxygen all
 template <class element_type>
-class element_caster : public std::conditional<std::is_const<element_type>::value, const_visitor, visitor>::type
+class element_caster : public std::conditional_t<std::is_const_v<element_type>, const_visitor, visitor>
 {
 	static_assert(
-		std::is_convertible<
+		std::is_convertible_v<
 			element_type*,
-			typename std::conditional<std::is_const<element_type>::value, const element*, element*>::type>::value,
+			typename std::conditional_t<std::is_const_v<element_type>, const element*, element*>>,
 		"element_type must inherit element"
 	);
 
@@ -53,8 +53,8 @@ public:
 	}
 
 	void default_visit(
-		typename std::conditional<std::is_const<element_type>::value, const element&, element&>::type e,
-		typename std::conditional<std::is_const<element_type>::value, const container&, container&>::type c
+		typename std::conditional_t<std::is_const_v<element_type>, const element&, element&> e,
+		typename std::conditional_t<std::is_const_v<element_type>, const container&, container&> c
 	) override
 	{
 		// do nothing
@@ -62,14 +62,14 @@ public:
 };
 
 template <bool is_const>
-class container_caster_template : public std::conditional<is_const, const_visitor, visitor>::type
+class container_caster_template : public std::conditional_t<is_const, const_visitor, visitor>
 {
 public:
-	typename std::conditional<is_const, const container*, container*>::type pointer = nullptr;
+	typename std::conditional_t<is_const, const container*, container*> pointer = nullptr;
 
 	void default_visit(
-		typename std::conditional<is_const, const element&, element&>::type e,
-		typename std::conditional<is_const, const container&, container&>::type c
+		typename std::conditional_t<is_const, const element&, element&> e,
+		typename std::conditional_t<is_const, const container&, container&> c
 	) override
 	{
 		this->pointer = &c;
@@ -100,127 +100,126 @@ inline const container* cast_to_container(const element* e)
 }
 
 template <bool is_const>
-class styleable_caster_template : public std::conditional<is_const, const_visitor, visitor>::type
+class styleable_caster_template : public std::conditional_t<is_const, const_visitor, visitor>
 {
 public:
-	typename std::conditional<is_const, const styleable*, styleable*>::type pointer = nullptr;
+	typename std::conditional_t<is_const, const styleable*, styleable*> pointer = nullptr;
 
-	void visit(typename std::conditional<is_const, const path_element&, path_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const path_element&, path_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const rect_element&, rect_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const rect_element&, rect_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const circle_element&, circle_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const circle_element&, circle_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const ellipse_element&, ellipse_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const ellipse_element&, ellipse_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const line_element&, line_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const line_element&, line_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const polyline_element&, polyline_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const polyline_element&, polyline_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const polygon_element&, polygon_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const polygon_element&, polygon_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const g_element&, g_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const g_element&, g_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const svg_element&, svg_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const svg_element&, svg_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const symbol_element&, symbol_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const symbol_element&, symbol_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const use_element&, use_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const use_element&, use_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const defs_element&, defs_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const defs_element&, defs_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const gradient::stop_element&, gradient::stop_element&>::type e
+	void visit(typename std::conditional_t<is_const, const gradient::stop_element&, gradient::stop_element&> e) override
+	{
+		this->pointer = &e;
+	}
+
+	void visit(typename std::conditional_t<is_const, const linear_gradient_element&, linear_gradient_element&> e
 	) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const linear_gradient_element&, linear_gradient_element&>::type e
+	void visit(typename std::conditional_t<is_const, const radial_gradient_element&, radial_gradient_element&> e
 	) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const radial_gradient_element&, radial_gradient_element&>::type e
+	void visit(typename std::conditional_t<is_const, const filter_element&, filter_element&> e) override
+	{
+		this->pointer = &e;
+	}
+
+	void visit(typename std::conditional_t<is_const, const fe_gaussian_blur_element&, fe_gaussian_blur_element&> e
 	) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const filter_element&, filter_element&>::type e) override
-	{
-		this->pointer = &e;
-	}
-
-	void visit(typename std::conditional<is_const, const fe_gaussian_blur_element&, fe_gaussian_blur_element&>::type e
+	void visit(typename std::conditional_t<is_const, const fe_color_matrix_element&, fe_color_matrix_element&> e
 	) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const fe_color_matrix_element&, fe_color_matrix_element&>::type e
-	) override
+	void visit(typename std::conditional_t<is_const, const fe_blend_element&, fe_blend_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const fe_blend_element&, fe_blend_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const fe_composite_element&, fe_composite_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const fe_composite_element&, fe_composite_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const image_element&, image_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const image_element&, image_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const mask_element&, mask_element&> e) override
 	{
 		this->pointer = &e;
 	}
 
-	void visit(typename std::conditional<is_const, const mask_element&, mask_element&>::type e) override
-	{
-		this->pointer = &e;
-	}
-
-	void visit(typename std::conditional<is_const, const text_element&, text_element&>::type e) override
+	void visit(typename std::conditional_t<is_const, const text_element&, text_element&> e) override
 	{
 		this->pointer = &e;
 	}
